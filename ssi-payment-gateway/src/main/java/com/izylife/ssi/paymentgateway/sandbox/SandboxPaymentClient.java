dc2780faecf371b81a0755a315bff0e33aacb3a3
Giuseppe Cassata <giuseppe.cassata74@gmail.com>
2026-03-12 13:32:54 +0100

    Initial commit

--- ssi-payment-gateway/src/main/java/com/izylife/ssi/paymentgateway/sandbox/SandboxPaymentClient.java
+++
@@ -1,9 +0,0 @@
-package com.izylife.ssi.paymentgateway.sandbox;
-
-import com.izylife.ssi.paymentgateway.model.PaymentTransaction;
-import com.izylife.ssi.paymentgateway.service.CredentialPaymentData;
-
-public interface SandboxPaymentClient {
-
-    SandboxPaymentResult authorize(PaymentTransaction transaction, CredentialPaymentData credentialData);
-}
--- ssi-payment-gateway/src/main/java/com/izylife/ssi/paymentgateway/service/PkcePair.java
+++
@@ -1,4 +0,0 @@
-package com.izylife.ssi.paymentgateway.service;
-
-public record PkcePair(String codeVerifier, String codeChallenge, String method) {
-}
