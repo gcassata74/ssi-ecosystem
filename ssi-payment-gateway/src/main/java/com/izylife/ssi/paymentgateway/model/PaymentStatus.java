ea98446eaeba0c0934896a32b99233964d6c4bbb
Giuseppe Cassata <giuseppe.cassata74@gmail.com>
2026-03-12 13:32:54 +0100

    Initial commit

--- ssi-payment-gateway/src/main/java/com/izylife/ssi/paymentgateway/model/PaymentStatus.java
+++
@@ -1,9 +0,0 @@
-package com.izylife.ssi.paymentgateway.model;
-
-public enum PaymentStatus {
-    CREATED,
-    AUTHORIZATION_REQUIRED,
-    CREDENTIAL_VERIFIED,
-    SANDBOX_CONFIRMED,
-    FAILED
-}
--- ssi-payment-gateway/src/main/java/com/izylife/ssi/paymentgateway/service/PaymentCallbackResult.java
+++
@@ -1,8 +0,0 @@
-package com.izylife.ssi.paymentgateway.service;
-
-import com.izylife.ssi.paymentgateway.model.PaymentTransaction;
-
-import java.net.URI;
-
-public record PaymentCallbackResult(PaymentTransaction transaction, URI redirectUri) {
-}
